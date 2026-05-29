900c5eced8e739ec4103a1d4df0d679f122cbea3
Jose Eduardo Rojas Jimenez <joseeduardox@gmail.com>
2026-05-28 18:25:32 -0600

    chore(infra): add missing TestConfig and finalized router fixes

---
+++ router/test/test_config.dart
@@ -0,0 +1,16 @@
+import 'package:seedsphere_core/seedsphere_core.dart';
+
+class TestConfig implements AppConfig {
+  @override String get torznabUrl => '';
+  @override Future<String?> getTorznabKey() async => null;
+  @override bool get enableTorznab => false;
+  @override String get trackerVariant => 'best';
+  @override String get customTrackersUrl => '';
+  @override bool get probeTrackers => false;
+  @override bool get probeProviders => false;
+  @override String get validationMode => 'basic';
+  @override int get probeTimeoutMs => 5000;
+  @override int get providerFetchTimeoutMs => 10000;
+  @override int get maxResultsPerProvider => 20;
+  @override bool get enableTrackerScraping => false;
+}
